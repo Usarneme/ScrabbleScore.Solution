07f2be6844a95b291c18e13b3d309174e7d4a65b
Usarneme <jamestcoop@gmail.com>
2021-05-06 11:14:04 -0700

    :white_check_mark: Pass - adds tests for all letters in the alphabet via 'the quick brown fox...'
    Co-authored-by: Brandon Magofna <Magofna68@gmail>
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>

--- ScrabbleScore.Tests/ModelTests/ScrabbleScoreTest.cs
+++ ScrabbleScore.Tests/ModelTests/ScrabbleScoreTest.cs
@@ -28,5 +28,117 @@ namespace ScrabbleScoreTesting
       int returnedScore = ScrabbleScore.GetWordScore(word);
       Assert.AreEqual(expectedScore, returnedScore);
     }
+
+    [TestMethod]
+    public void GetWordScore_The_Six()
+    {
+      // Arrange
+      string word = "the";
+      int expectedScore = 6;
+
+      // Act
+      int returnedScore = ScrabbleScore.GetWordScore(word);
+
+      // Assert
+      Assert.AreEqual(expectedScore, returnedScore);
+    }
+
+    [TestMethod]
+    public void GetWordScore_Quick_Twenty()
+    {
+      // Arrange
+      string word = "quick";
+      int expectedScore = 20;
+
+      // Act
+      int returnedScore = ScrabbleScore.GetWordScore(word);
+
+      // Assert
+      Assert.AreEqual(expectedScore, returnedScore);
+    }
+
+    [TestMethod]
+    public void GetWordScore_Brown_Ten()
+    {
+      // Arrange
+      string word = "brown";
+      int expectedScore = 10;
+
+      // Act
+      int returnedScore = ScrabbleScore.GetWordScore(word);
+
+      // Assert
+      Assert.AreEqual(expectedScore, returnedScore);
+    }
+
+    [TestMethod]
+    public void GetWordScore_Fox_Thirteen()
+    {
+      // Arrange
+      string word = "fox";
+      int expectedScore = 13;
+
+      // Act
+      int returnedScore = ScrabbleScore.GetWordScore(word);
+
+      // Assert
+      Assert.AreEqual(expectedScore, returnedScore);
+    }
+
+    [TestMethod]
+    public void GetWordScore_Jumps_Sixteen()
+    {
+      // Arrange
+      string word = "jumps";
+      int expectedScore = 16;
+
+      // Act
+      int returnedScore = ScrabbleScore.GetWordScore(word);
+
+      // Assert
+      Assert.AreEqual(expectedScore, returnedScore);
+    }
+
+    [TestMethod]
+    public void GetWordScore_Over_Seven()
+    {
+      // Arrange
+      string word = "over";
+      int expectedScore = 7;
+
+      // Act
+      int returnedScore = ScrabbleScore.GetWordScore(word);
+
+      // Assert
+      Assert.AreEqual(expectedScore, returnedScore);
+    }
+
+    [TestMethod]
+    public void GetWordScore_Lazy_Sixteen()
+    {
+      // Arrange
+      string word = "lazy";
+      int expectedScore = 16;
+
+      // Act
+      int returnedScore = ScrabbleScore.GetWordScore(word);
+
+      // Assert
+      Assert.AreEqual(expectedScore, returnedScore);
+    }
+
+    [TestMethod]
+    public void GetWordScore_Dog_Five()
+    {
+      // Arrange
+      string word = "dog";
+      int expectedScore = 5;
+
+      // Act
+      int returnedScore = ScrabbleScore.GetWordScore(word);
+
+      // Assert
+      Assert.AreEqual(expectedScore, returnedScore);
+    }
   }
 }
