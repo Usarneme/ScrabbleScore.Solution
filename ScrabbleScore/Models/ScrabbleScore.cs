3e0d9f03d7194fe200a1a2d6141c092868a21090
Usarneme <jamestcoop@gmail.com>
2021-05-06 10:53:05 -0700

    :white_check_mark: Pass - adds functionality to pass first test
    Co-authored-by: Brandon Magofna <Magofna68@gmail>
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>

--- ScrabbleScore/Models/ScrabbleScore.cs
+++ ScrabbleScore/Models/ScrabbleScore.cs
@@ -1,10 +1,38 @@
+using System.Collections.Generic;
 namespace ScrabbleScoreNamespace
 {
   public class ScrabbleScore
   {
+
+    // A, E, I, O, U, L, N, R, S, T       1
+    // D, G                               2
+    // B, C, M, P                         3
+    // F, H, V, W, Y                      4
+    // K                                  5
+    // J, X                               8
+    // Q, Z                               10
+
+    private static Dictionary<char, int> _scoreSheet = new Dictionary<char, int>()
+    {
+      {'a', 1}
+    };
+
+    private static int GetLetterScore(char letter)
+    {
+      int characterScore = _scoreSheet[letter];
+      return characterScore;
+    }
+
     public static int GetWordScore(string word)
     {
-      return 0;
+      int totalScore = 0;
+
+      foreach (char letter in word)
+      {
+        totalScore += GetLetterScore(letter);
+      }
+
+      return totalScore;
     }
   }
 }
